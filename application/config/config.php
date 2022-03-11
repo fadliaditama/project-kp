4928501f3995cf5dc5421ec2e2193ef905e7b47a
Acmad Fadli Aditama <37434467+fadliaditama@users.noreply.github.com>
2022-03-11 15:22:16 +0700

    Edit Base_URL
    Change route and base_URL

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/final/';
+$config['base_url'] = 'http://localhost/project-kp';
 
 /*
 |--------------------------------------------------------------------------
